bd04446ad055115aa42e4a03b212d8f0014d0972
Abdelrahman <abdelrahman.ayman8642@gmail.com>
2023-10-25 22:23:31 +0300

    create view posts page

--- api/index.js
+++ api/index.js
@@ -18,6 +18,7 @@ const app = express();
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(cookieParser());
+app.use("/uploads", express.static("uploads"));
 
 mongoose.connect(
   "mongodb+srv://abdelrahmanayman8642:ru5phBlPjG8IJjEB@cluster0.fymrzt6.mongodb.net/"
@@ -87,6 +88,15 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
     });
     res.json(postDoc);
   });
+});
+
+app.get("/post", async (req, res) => {
+  res.json(
+    await Post.find()
+      .populate("author", ["username"])
+      .sort({ createdAt: -1 })
+      .limit(20)
+  );
 });
 
 app.listen(4000);
--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import LoginPage from "./pages/LoginPage";
+import IndexPage from "./pages/IndexPage";
 import RegisterPage from "./pages/RegisterPage";
 import { UserContextProvider } from "./UserContext";
 import CreatePost from "./pages/CreatePost";
@@ -11,6 +12,7 @@ function App() {
     <UserContextProvider>
       <Routes>
         <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/create" element={<CreatePost />} />
